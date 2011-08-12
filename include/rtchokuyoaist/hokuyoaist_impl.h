57ace2c31c8fc73a070e057a6cafb6ac21aeb650
Geoffrey Biggs <git@killbots.net>
2011-08-12 14:16:12 +0900

    Updated license headers

--- include/rtchokuyoaist/hokuyoaist_impl.h
+++ include/rtchokuyoaist/hokuyoaist_impl.h
@@ -1,16 +1,29 @@
-/* RTC:HokuyoAist
+/* RTC:HokuyoAIST
  *
- * Copyright (C) 2010
- *     Geoffrey Biggs
+ * Header file for the HokuyoAIST interface implementation.
+ *
+ * Copyright 2010-2011 Geoffrey Biggs geoffrey.biggs@aist.go.jp
  *     RT-Synthesis Research Group
  *     Intelligent Systems Research Institute,
  *     National Institute of Advanced Industrial Science and Technology (AIST),
  *     Japan
  *     All rights reserved.
- * Licensed under the Eclipse Public License -v 1.0 (EPL)
- * http://www.opensource.org/licenses/eclipse-1.0.txt
  *
- * Service implementation class.
+ * This file is part of HokuyoAIST.
+ *
+ * HokuyoAIST is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 2.1 of the License,
+ * or (at your option) any later version.
+ *
+ * HokuyoAIST is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with HokuyoAIST. If not, see
+ * <http://www.gnu.org/licenses/>.
  */
 
 #if !defined(HOKUYOAIST_IMPL__)
